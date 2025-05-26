f71d6fd0a4e921fd424cce817261c15c595b3cf2
Rutik Gurav <rutikgurav15@gmail.com>
2025-05-26 20:55:57 +0530

    Add files via upload

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 12, b = 13;
+
+            //Arithmetic
+            Console.WriteLine("a+b = " + (a + b));
+            Console.WriteLine("a-b = " + (a - b));
+            Console.WriteLine("a*b = " + (a * b));
+            Console.WriteLine("a/b = " + (a / b));
+            Console.WriteLine("a%b = " + (a % b));
+
+            //Relational
+            Console.WriteLine("a>b : " + (a>b));
+            Console.WriteLine("a>=b : " + (a >= b));
+            Console.WriteLine("a<b : " + (a < b));
+            Console.WriteLine("a<b : " + (a <= b));
+            Console.WriteLine("a==b : " + (a == b));
+
+            //Logical
+            Console.WriteLine("a==b && b!=a : " + (a==b && b!=a));
+            Console.WriteLine("a>=b || a >=b : " + (a>=b || a >=b));
+            Console.WriteLine("a != b : " + (a != b));
+
+            //Assignment
+            Console.WriteLine("a = a+b : "+ (a = a + b));
+            Console.WriteLine("a +=b : "+(a += b));
+            Console.WriteLine("a -=b : "+(a -= b));
+            Console.WriteLine("a *=b : "+(a *= b));
+            Console.WriteLine("a /=b : "+(a /= b));
+
+            //Increment-Decrement
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+            Console.WriteLine(b++);
+            Console.WriteLine(++b);
+            Console.WriteLine(b--);
+            Console.WriteLine(--b);
+
+            //
+            int message = (a > b) ? a : b;
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Parsing/Parsing/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace TypeConversion
+{
+    class TypeConversion
+    {
+        static void Main(string[] args)
+        {
+            // By Parsing
+
+            string str1 = "100";
+            int num1 = int.Parse(str1);
+            Console.WriteLine(num1);
+
+            string str2 = "100.00";
+            float num2 = float.Parse(str2);
+            Console.WriteLine(num2);
+
+            string str3 = "true";
+            bool num3 = bool.Parse(str3);
+            Console.WriteLine(num3);
+
+
+
+            // By Convert class
+
+            string str4 = "false";
+            bool num4 = Convert.ToBoolean(str4);
+            Console.WriteLine(num4);
+
+            string str5 = "2823233892839983";
+            long num5 = Convert.ToInt64(str5);
+            Console.WriteLine(num5);
+
+
+            // Implicitly typed variable
+
+            var name = "Rutik";
+            var age = 23;
+
+            var n1 = 10;
+            var n2 = 20;
+            var n3 = n1 + n2;
+
+            Console.WriteLine(name+" "+age);
+            Console.WriteLine(n1+" "+n2+" "+n3);
+
+
+            Console.ReadKey();
+        }
+    }
+}
