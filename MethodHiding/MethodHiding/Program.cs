03b2335927fb78ea57a05b997097d6af9ce4e2fd
Rutik Gurav <rutikgurav15@gmail.com>
2025-05-26 20:53:33 +0530

    Add files via upload

---
+++ MethodHiding/MethodHiding/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace MethodHiding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Dog();
+            animal.Hii();
+            animal.Hello();
+
+            Dog dog = new Dog();
+            dog.Hii();
+            dog.Hello();
+
+            Console.ReadKey();
+        }
+
+        
+    }
+
+    class Animal
+    {
+        public virtual void Hii()
+        {
+            Console.WriteLine("Overriding -> I'm from Base class.");
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("Hiding -> I'm from Base class.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Hii()
+        {
+            Console.WriteLine("Overriding -> I'm from Derived class.");
+        }
+
+        public new void Hello()
+        {
+            Console.WriteLine("Hiding -> I'm from Derived class.");
+        }
+    }
+}
---
+++ MultipleInheritanceExample/MultipleInheritanceExample/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MultipleInheritanceExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flyable flyable = new Duck();
+            Swimmable swimmable = new Duck();
+
+            flyable.fly();
+            swimmable.swim();
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface Flyable
+    {
+        void fly();
+    }
+
+    public interface Swimmable
+    {
+        void swim();
+    }
+
+    class Duck: Flyable, Swimmable
+    {
+        public void fly()
+        {
+            Console.WriteLine("Duck can fly..");
+
+        }
+
+        public void swim()
+        {
+            Console.WriteLine("Duck can swim..");
+        }
+    }
+}
