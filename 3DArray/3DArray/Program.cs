89a2468941be3b4cdcfa41f83641aaf1bed90ea0
Rutik Gurav <rutikgurav15@gmail.com>
2025-05-26 20:22:59 +0530

    Add files via upload

---
+++ 3DArray/3DArray/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _3DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+
+            Console.WriteLine("Enter 3D array elements : ");
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    
+                        arr[i, j] = int.Parse(Console.ReadLine());
+                    
+                }
+            }
+
+            Console.WriteLine("\n3D array : \n");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    
+                        Console.Write(arr[i,j]+"  ");
+                   
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
