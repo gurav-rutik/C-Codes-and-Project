cb407ad0c5a3e743603e9ebe0d09be0c26c6c73f
Rutik Gurav <rutikgurav15@gmail.com>
2025-05-26 20:49:05 +0530

    Add files via upload

---
+++ IfStatement/IfStatement/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age : ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine($"Your age is {age}!! Your eligible for voting!!");
+            }
+
+            Console.WriteLine("Bye!!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ImplicitConversion/ImplicitConversion/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DataTypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Type casting
+
+            byte b = 10;
+            short s = b;
+            int i = s;
+            long l = i;
+            float f = l;
+            double d = l;
+            Console.WriteLine(d);
+
+            int num1 = 12, num2=13;
+
+            double result = num1 + num2;
+
+            result = Math.Round(result, 2);
+            Console.WriteLine(result);
+
+            // Explicit Type casting
+
+            double d1 = 123.1435352324;
+
+            float f1 = (float) d1;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(f1);
+
+            long l1 = 5473798118635371812;
+            int i2 = (int)l1;
+
+            Console.WriteLine(l1);
+            Console.WriteLine(i2);
+
+            Console.ReadKey();
+        }
+    }
+}
