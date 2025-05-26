b133dc769e976389022e74ed9e2f493c70c6ff49
Rutik Gurav <rutikgurav15@gmail.com>
2025-05-26 21:01:12 +0530

    Add files via upload

---
+++ SealedMethod/SealedMethod/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SealedMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.talk();
+
+            Dog dog = new Dog();
+            dog.talk();
+
+            Cat cat = new Cat();
+            cat.talk();
+
+            Animal animal1 = new Dog();
+            animal1.talk();
+
+            Dog dog1 = new Cat();
+            dog1.talk();
+
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void talk()
+        {
+            Console.WriteLine("Animal makes sound!");
+        }
+    }
+    class Dog:Animal
+    {
+        public sealed override void talk()
+        {
+            Console.WriteLine("Dog makes sound! : bow-bow");
+        }
+    }
+    class Cat:Dog
+    {
+        // can't override it, because sealed
+
+        //public override void talk()
+        //{
+        //    Console.WriteLine("Cat makes sound! : meow-meow");
+        //}
+    }
+}
---
+++ SpecialCharacterInString/SpecialCharacterInString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SpecialCharacterInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "My hobbies are \"Playing Cricket\" \\ \"Drawing\"!!";
+            Console.WriteLine(str);
+
+            string str2 = "Hello\n\t Rutik!!";
+            Console.WriteLine(str2);
+
+            Console.ReadKey();
+        }
+    }
+}
