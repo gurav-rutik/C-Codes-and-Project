486e6674869356e5be6a887622a37dad0b1748cd
Rutik Gurav <rutikgurav15@gmail.com>
2025-05-26 20:28:35 +0530

    Add files via upload

---
+++ CallStachExample/CallStachExample/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CallStachExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LevelOne();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Appp is still running..");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void LevelOne()
+        {
+            LevelTwo();
+
+        }
+
+        public static void LevelTwo()
+        {
+            Console.WriteLine("Level two before throw");
+            throw new Exception("Something went wrong!!");
+        }
+    }
+}
---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Comments
+{
+    class Comment
+    {
+        public static void Main(string[] args)
+        {
+            // Single Line Comments
+            // we are assigning 1 value to a
+            int a = 1;
+
+            // Multi-Line Comments
+            /*We adding a with 5*/
+            int b = a + 5;
+
+            int sum = printSum(a, b);
+
+
+        }
+
+        // XML documentation comment
+        ///<summary>
+            ///Retuning the sum of a and b
+            ///</summary>
+        public static int printSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
